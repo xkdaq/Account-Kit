3d52a8e0c623930335a1a52a5463913999920f6f
xuke <228619770@qq.com>
2019-06-27 16:23:09 +0800

    [commit]login

--- app/src/main/java/com/account/kit/MainActivity.java
+++ app/src/main/java/com/account/kit/MainActivity.java
@@ -1,13 +1,97 @@
 package com.account.kit;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.facebook.accountkit.AccessToken;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitLoginResult;
+import com.facebook.accountkit.ui.AccountKitActivity;
+import com.facebook.accountkit.ui.AccountKitConfiguration;
+import com.facebook.accountkit.ui.LoginType;
+import com.facebook.appevents.AppEventsLogger;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int APP_REQUEST_CODE = 99;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // check for an existing access token
+        AccessToken accessToken = AccountKit.getCurrentAccessToken();
+        if (accessToken != null) {
+            // if previously logged in, proceed to the account activity
+            Log.e("xuke", "accessToken = " + accessToken);
+            launchAccountActivity();
+        } else {
+            Log.e("xuke", "accessToken = null");
+        }
+    }
+
+    public void onPhoneLogin(View view) {
+        AppEventsLogger logger = AppEventsLogger.newLogger(this);
+        logger.logEvent("onPhoneLogin");
+
+        onLogin(LoginType.PHONE);
+    }
+
+    public void onEmailLogin(View view) {
+        AppEventsLogger logger = AppEventsLogger.newLogger(this);
+        logger.logEvent("onEmailLogin");
+
+        onLogin(LoginType.EMAIL);
+    }
+
+
+    private void onLogin(final LoginType loginType) {
+        // create intent for the Account Kit activity
+        final Intent intent = new Intent(this, AccountKitActivity.class);
+
+        // configure login type and response type
+        AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder =
+                new AccountKitConfiguration.AccountKitConfigurationBuilder(
+                        loginType,
+                        AccountKitActivity.ResponseType.TOKEN
+                );
+        //configurationBuilder.setInitialPhoneNumber(new PhoneNumber())
+        final AccountKitConfiguration configuration = configurationBuilder.build();
+
+        // launch the Account Kit activity
+        intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION, configuration);
+        startActivityForResult(intent, APP_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // confirm that this response matches your request
+        if (requestCode == APP_REQUEST_CODE) {
+            AccountKitLoginResult loginResult = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
+            Log.e("xuke", "loginResult=" + loginResult);
+            Log.e("xuke", "loginResult=" + loginResult.getError());
+            if (loginResult.getError() != null) {
+                // display login error
+                String toastMessage = loginResult.getError().getErrorType().getMessage();
+                Toast.makeText(this, toastMessage, Toast.LENGTH_LONG).show();
+            } else if (loginResult.getAccessToken() != null) {
+                // on successful login, proceed to the account activity
+                launchAccountActivity();
+            }
+        }
+    }
+
+
+    private void launchAccountActivity() {
+        Intent intent = new Intent(this, AccountActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
