a03bf53c5452c38383f9dac29feef827e3eafd24
xuke <228619770@qq.com>
2019-06-27 16:36:26 +0800

    [commit]log out

--- app/src/main/java/com/account/kit/AccountActivity.java
+++ app/src/main/java/com/account/kit/AccountActivity.java
@@ -1,14 +1,91 @@
 package com.account.kit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.accountkit.Account;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitCallback;
+import com.facebook.accountkit.AccountKitError;
+import com.facebook.accountkit.PhoneNumber;
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.Phonenumber;
+
+import java.util.Locale;
 
 public class AccountActivity extends AppCompatActivity {
+
+    private TextView tvId;
+    private TextView tvAccount;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        tvId = (TextView) findViewById(R.id.tv_id);
+        tvAccount = (TextView) findViewById(R.id.tv_account);
+
+
+        AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
+            @Override
+            public void onSuccess(final Account account) {
+                // Get Account Kit ID
+                String accountKitId = account.getId();
+                tvId.setText(String.format(getString(R.string.text_id), accountKitId));
+
+                PhoneNumber phoneNumber = account.getPhoneNumber();
+                if (account.getPhoneNumber() != null) {
+                    // if the phone number is available, display it
+                    String formattedPhoneNumber = formatPhoneNumber(phoneNumber.toString());
+                    tvAccount.setText(String.format(getString(R.string.text_account), formattedPhoneNumber));
+                } else {
+                    // if the email address is available, display it
+                    String emailString = account.getEmail();
+                    tvAccount.setText(String.format(getString(R.string.text_account), emailString));
+                }
+            }
+
+            @Override
+            public void onError(final AccountKitError error) {
+                // display error
+                String toastMessage = error.getErrorType().getMessage();
+                Toast.makeText(AccountActivity.this, toastMessage, Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    /**
+     * log out
+     */
+    public void onLogout(View view) {
+        // logout of Account Kit
+        AccountKit.logOut();
+        launchLoginActivity();
+    }
+
+    private void launchLoginActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private String formatPhoneNumber(String phoneNumber) {
+        // helper method to format the phone number for display
+        try {
+            PhoneNumberUtil pnu = PhoneNumberUtil.getInstance();
+            Phonenumber.PhoneNumber pn = pnu.parse(phoneNumber, Locale.getDefault().getCountry());
+            phoneNumber = pnu.format(pn, PhoneNumberUtil.PhoneNumberFormat.NATIONAL);
+        } catch (NumberParseException e) {
+            e.printStackTrace();
+        }
+        return phoneNumber;
     }
 }
